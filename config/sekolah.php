e095f811726f12a533c073c71b50decdefd08970
Philip Loa <boyet007@gmail.com>
2019-11-12 01:05:59 +0700

    part 27 - Setting website

---
+++ config/sekolah.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'logo_url' => '/images/logo.png',
+    'title' => 'SMA BPK Penabur',
+    'welcome_message' => 'Selamat datang di Website SMA BPK Penabur',
+    'sub_welcome_message' => 'Sekolah dnegan kurikulum terbaik dijakarta',
+    'welcome_message_button_text' => 'Jelajahi',
+
+    'telpon' => '021 929 2911',
+    'email' => 'bpkpenabur@yahoo.com',
+];
